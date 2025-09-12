db0eee0491658cbedab48bc0af1226bf7b5d432b
zsb8 <songbin.zhang@gmail.com>
2025-09-12 08:27:10 -0400

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,10 @@
-import React from "react";
-import type { AppProps } from "next/app";
-import { ConfigProvider } from "antd";
-import zhCN from "antd/locale/zh_CN";
 import "antd/dist/reset.css";
 import "../styles/globals.css";
-import { CartProvider } from "../contexts/CartContext";
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = () => {
   return (
-    <ConfigProvider locale={zhCN}>
-      <CartProvider>
-        <Component {...pageProps} />
-      </CartProvider>
-    </ConfigProvider>
+    <>
+    </>
   );
 };
 
